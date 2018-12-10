d4dc9519e2411e53d764c00555d8701ed9b06414
truongthevinh122 <45725610+truongthevinh122@users.noreply.github.com>
2018-12-10 20:58:12 +0700

    Add files via upload

---
+++ week2/fibonacci_last_digit.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int get_fibonacci_last_digit_naive(int n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current  = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+    }
+
+    return current % 10;
+}
+int get_fibonacci_last_digit_fast(int n){
+	int a1 = 0;
+    int a2 = 1;
+
+    int result;
+
+    for (int i = 2; i <= n; i++) {
+        result = (a1 + a2) % 10;
+        a1 = a2;
+        a2 = result;
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int c = get_fibonacci_last_digit_naive(n);
+    std::cout << c << '\n';
+    }
---
+++ week2/lcm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long) a * b;
+}
+
+int lcm_fast(int a, int b)
+{ 
+    int temp = 1;
+ 
+    if (temp % a == 0 && temp % b == 0)
+    {
+        return temp;
+    }
+    common++;
+    lcm(a, b);
+    return temp;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << lcm_naive(a, b) << std::endl;
+  return 0;
+}
