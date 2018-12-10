047fac9715470c28882bd5bbdac7c704a12200ad
truongthevinh122 <45725610+truongthevinh122@users.noreply.github.com>
2018-12-10 21:02:57 +0700

    Update gcd.cpp

--- week2/gcd.cpp
+++ week2/gcd.cpp
@@ -26,6 +26,6 @@ int gcd_fast(int a, int b)
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }
