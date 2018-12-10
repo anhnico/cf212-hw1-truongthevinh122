03b39d632d70ded53c7adbe1063617519a0698eb
truongthevinh122 <45725610+truongthevinh122@users.noreply.github.com>
2018-12-10 21:03:24 +0700

    Update lcm.cpp

--- week2/lcm.cpp
+++ week2/lcm.cpp
@@ -24,6 +24,6 @@ int lcm_fast(int a, int b)
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
